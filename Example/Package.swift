479bdcd62e421faa07dd7f3bd6a5b68ad25e1397
Ron Kliffer <ronksa@gmail.com>
2025-10-30 15:40:10 +0200

    Update iOS platform version to v16

--- Example/Package.swift
+++ Example/Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "DemarkExample",
     platforms: [
-        .macOS(.v14),
-        .iOS(.v17),
+        .iOS(.v16),
     ],
     products: [
         .executable(
