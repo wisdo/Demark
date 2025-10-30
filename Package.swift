bedb34da37ffa6cf5cda9f30079f7a72bcea1613
Ron Kliffer <ronksa@gmail.com>
2025-10-30 15:39:46 +0200

    Modify supported platforms in Package.swift
    Updated iOS platform version to v16 and removed other platforms.

--- Package.swift
+++ Package.swift
@@ -6,11 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Demark",
     platforms: [
-        .macOS(.v14),
-        .iOS(.v17),
-        .watchOS(.v10),
-        .tvOS(.v17),
-        .visionOS(.v1)
+        .iOS(.v16)
     ],
     products: [
         .library(
